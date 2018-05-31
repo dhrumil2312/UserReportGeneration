bb0bf6e59f2d58d94c77ef23c2e49cf9392ea25b
dhrumil2312 <dhrumil2312@gmail.com>
2018-05-31 10:15:32 -0700

    Readme and Changes

--- user_upload_dump_backup.sh
+++ user_upload_dump_backup.sh
@@ -2,12 +2,13 @@
 
 # /user/cloudera/exercise1/userlogs/ should exists before running this script if not run below commands
 
-# hadoop fs -mkdir /user/cloudera/exercise1/
-# hadoop fs -mkdir /user/cloudera/exercise1/userlogs/
+# hadoop fs -mkdir /app/
+# hadoop fs -mkdir /app/data/
+# hadoop fs -mkdir /app/data/user_upload/
 
 for filename in ./*csv ; do
-hadoop fs -put $filename /user/cloudera/exercise1/userlogs/
+hadoop fs -put $filename /app/data/user_upload/
 done
 
 
-hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS user_upload_dump (user_id int, file_name STRING,     timestamp STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '/user/cloudera/exercise1/userlogs/' TBLPROPERTIES ('skip.header.line.count'='1');"
+hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS user_upload_dump (user_id int, file_name STRING,     timestamp STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE LOCATION '/app/data/user_upload/' TBLPROPERTIES ('skip.header.line.count'='1');"
